372d6865e99981273d96cc9c0ab40f880247e50d
rdhammack88 <rdhammack88@hotmail.com>
2020-09-30 18:39:29 -0500

    Created and modified an index file for a replacement to the built-in bookmark manager page.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /**
  * Recursive call to retrieve all bookmarks and bookmark folders
  * Initial call in getBookmarks()
- * @param {obj} bookmarkData 
- * @param {obj} bookmarks 
+ * @param {obj} bookmarkData
+ * @param {obj} bookmarks
  */
 const scanBookmarks = (bookmarkData, bookmarks) => {
     if (bookmarks.url) {
@@ -14,8 +14,9 @@ const scanBookmarks = (bookmarkData, bookmarks) => {
         bookmarks.children.forEach((bookmark) => {
             bookmarkData.totalFolders++;
             bookmarkData.bookmarkFolders.push(bookmarks);
+            // document.getElementById('bookmarkList').innerHTML += `<li>${bookmark.title}</li>`;
             return scanBookmarks(bookmarkData, bookmark);
-        })
+        });
     }
     return bookmarkData;
 };
@@ -30,8 +31,10 @@ const getBookmarks = () => {
         totalBookmarks: 0,
         totalFolders: 0,
         bookmarkFolders: [],
-        bookmarks: []
+        bookmarks: [],
     };
+
+    // document.getElementById('test').innerText = 'This is a test from the JS file....';
 
     chrome.bookmarks.getTree((bookmarks) => {
         console.log('Bookmarks: ', bookmarks);
@@ -42,17 +45,52 @@ const getBookmarks = () => {
 
         console.log('Finished Scanning Bookmarks!');
         console.log('Bookmark Data: ', bookmarkData);
-        const bookmarkDataStringified = JSON.stringify(bookmarkData);
+        // const bookmarkDataStringified = JSON.stringify(bookmarkData);
         // console.log('Bookmark Data Stringified: ', JSON.stringify(bookmarkData));
 
         // console.log('Bookmark Data Stringified: ', JSON.stringify(bookmarkData));
 
-        document.getElementById('total-bookmarks').innerHTML = bookmarkData.totalBookmarks;
-        document.getElementById('total-folders').innerHTML = bookmarkData.totalFolders;
-
-
-
+        document.getElementById('total-bookmarks').innerText = bookmarkData.totalBookmarks;
+        document.getElementById('total-folders').innerText = bookmarkData.totalFolders;
     });
 };
 
+/**
+ * searchBookmarks
+ */
+const searchBookmarks = (query = '') => {
+    const bookmarkData = {
+        bookmarks: [],
+        duplicateBookmarks: [],
+    };
+    chrome.bookmarks.search(query, (bookmarkNodes) => {
+        console.log('Searching for bookmarks that include: ', query);
+        console.log('Bookmark Node Length: ', bookmarkNodes.length);
+        console.log('Bookmark Nodes: ', bookmarkNodes);
+
+        bookmarkNodes.forEach((node) => {
+            if (!bookmarkData.bookmarks.includes(node.url)) {
+                bookmarkData.bookmarks.push(node.url);
+            } else if (bookmarkData.bookmarks.includes(node.url)) {
+                bookmarkData.duplicateBookmarks.push(node.url);
+            }
+        });
+
+        console.log('Bookmarks length: ', bookmarkData.bookmarks);
+        console.log('Duplicate Bookmarks length: ', bookmarkData.duplicateBookmarks);
+    });
+};
+
+const getDuplicateFolders = () => {};
+
+const getDuplicateBookmarks = () => {};
+
 // getBookmarks();
+
+document.addEventListener('DOMContentLoaded', getBookmarks);
+document.getElementById('search').addEventListener('click', (e) => {
+    e.preventDefault();
+    const query = document.getElementById('query').value;
+    console.log(query);
+    searchBookmarks(query);
+});
